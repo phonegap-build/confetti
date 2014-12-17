c0488ee6460a0c870e78d9305d81e6f2bc07c370
Les Orchard <me@lmorchard.com>
2014-12-17 18:03:12 -0500

    [firefoxos] Add support for Firefox OS and JSON web app manifest

--- lib/confetti/config.rb
+++ lib/confetti/config.rb
@@ -12,7 +12,8 @@ module Confetti
                 :url_scheme_set, :platform_set
 
     generate_and_write  :android_manifest, :android_strings, :ios_info, 
-                        :ios_remote_plist, :windows_phone8_manifest
+                        :ios_remote_plist, :windows_phone8_manifest,
+                        :firefoxos_manifest
 
     # handle bad generate/write calls
     def method_missing(method_name, *args)
